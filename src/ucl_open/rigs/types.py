e1363fd4d0694e69ef5bed34d32f6757729a3dd8
NeuroThom <t.ryan@neurogears.org>
2025-12-10 16:10:46 +0000

    Add to DataTypes
    - SoftwareEvents
    - Vector3

--- src/ucl_open/rigs/types.py
+++ src/ucl_open/rigs/types.py
@@ -1,11 +1,9 @@
-from typing import Annotated
+from enum import StrEnum
+import json
+from pathlib import Path
+from typing import Annotated, Generic, TypeVar, Any
 from pydantic import Field
 from ucl_open.rigs.base import BaseSchema
-
-class Vector3(BaseSchema):
-    x: float = Field(description="X coordinate of the point")
-    y: float = Field(description="Y coordinate of the point")
-    z: float = Field(description="Z coordinate of the point")
 
 SByte = Annotated[int, Field(ge=-128, le=127)]
 Byte = Annotated[int, Field(ge=0, le=255)]
@@ -20,3 +18,36 @@ Float = float
 Double = float
 String = str
 Bool = bool
+
+class TimestampSource(StrEnum):
+    NULL = "null"
+    HARP = "harp"
+    RENDER = "render"
+    ARDUINO = "arduino"
+
+TData = TypeVar("TData", bound=Any)
+
+class Vector3(BaseSchema):
+    x: float = Field(description="X coordinate of the point.")
+    y: float = Field(description="Y coordinate of the point.")
+    z: float = Field(description="Z coordinate of the point.")
+
+class SoftwareEvent(BaseSchema, Generic[TData]):
+    """
+    A software event is a generic event that can be used to track any event that occurs in the software.
+    """
+    name: str = Field(..., description="The name of the event.")
+    timestamp: float | None = Field(default=None, description="The timestamp of the event.")
+    timestamp_source: TimestampSource = Field(default=TimestampSource.NULL,description="The source of the timestamp. Typically either a harp device or on the visual render loop")
+    frame_index: int | None = Field(default=None,ge=0,description="The frame index of the event.")
+    frame_timestamp: float | None = Field(default=None,description="The timestamp of the frame.")
+    data: TData | None = Field(default=None,description="The data payload of the event.")
+
+class DataTypes(BaseSchema):
+    vector3 : Vector3 
+    software_event : SoftwareEvent
+
+def main() -> None:
+    schema = DataTypes.model_json_schema(union_format="primitive_type_array")
+    schema.pop("properties", None)
+    Path("DataTypes.json").write_text(json.dumps(schema, indent=2))
